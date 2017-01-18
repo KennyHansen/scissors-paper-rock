1caa61438efca93f025ede2db84974911a32ca57
KennyHansen <hansen.kenny101@gmail.com>
2017-01-18 15:01:20 -0700

    added Dynamite

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ function play(choice) {
     var validChoices = {
         scissors: true,
         paper: true,
-        rock: true
+        rock: true,
+        dynamite: true
     }
 
     if (validChoices[choice]) {
@@ -25,6 +26,9 @@ function play(choice) {
 function findWinner(choice, computerChoice) {
     if (choice == computerChoice) {
         displayMessage("It's a tie!")
+    } else if (choice == 'dynamite') {
+        displayMessage("Kaboom!")
+        recordVictory('player')
     } else {
         var win = ((choice == 'scissors' && computerChoice == 'paper')
             || (choice == 'rock' && computerChoice == 'scissors')
@@ -40,7 +44,7 @@ function findWinner(choice, computerChoice) {
 }
 
 function displayMessage(message) {
-    document.getElementById('message-display').innerHTML = document.getElementById('message-display').innerHTML + ' <br> ' + message
+    document.getElementById('message-display').innerHTML += ' <br> ' + message
 }
 
 function recordVictory(winner) {
@@ -53,5 +57,18 @@ function updateWinPercent() {
     var playerWin = parseInt(document.getElementById('player-record').textContent)
     var compWin = parseInt(document.getElementById('computer-record').textContent)
 
-    document.getElementById('win-percent').textContent = Math.round(((playerWin)/(compWin + playerWin))* 100)
+    var winPercent = Math.round(((playerWin)/(compWin + playerWin))* 100)
+    document.getElementById('win-percent').textContent = winPercent
+    //updateFlavor(winPercent)
 }
+
+// function updateFlavor(winPercent) {
+//     if (winPercent > 60) {
+
+//     }
+
+//     else if () {
+
+//     }
+//     document.getElementById('flavor-img').innerHTML = 
+// }
